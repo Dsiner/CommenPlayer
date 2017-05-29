d5861664cef00a245ff93310c8805b03bcc3b8db
D <s90789@outlook.com>
2017-05-29 15:52:25 +0800

    add ijkplayer

--- app/src/main/java/com/d/commenplayer/MainActivity.java
+++ app/src/main/java/com/d/commenplayer/MainActivity.java
@@ -1,13 +1,39 @@
 package com.d.commenplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.d.commenplayer.widget.media.AndroidMediaController;
+import com.d.commenplayer.widget.media.IjkVideoView;
+import com.d.commenplayer.widget.preference.Settings;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
+    private AndroidMediaController mediaController;
+    private IjkVideoView player;
+    private Settings mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+
+        mSettings = new Settings(this);
+        player = (IjkVideoView) findViewById(R.id.player);
+        player.setVideoPath("http://vpls.cdn.videojj.com/scene/video02_720p.mp4");
+        player.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(IMediaPlayer iMediaPlayer) {
+                player.start();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/d/commenplayer/commen/CommenPlayer.java
@@ -0,0 +1,8 @@
+package com.d.commenplayer.commen;
+
+/**
+ * CommenPlayer
+ * Created by D on 2017/5/27.
+ */
+public class CommenPlayer {
+}
